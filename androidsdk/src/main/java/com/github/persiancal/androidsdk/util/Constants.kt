dac6ff62640699e1c8c497580c6a698e6e59cdaf
Behrouz <bkhezry@gmail.com>
2019-10-15 14:17:07 +0330

    add util class for handle retrofit instance - create Constants class for holding constants values - add base url of github repo for json files

---
+++ androidsdk/src/main/java/com/github/persiancal/androidsdk/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.github.persiancal.androidsdk.util
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://raw.githubusercontent.com/persiancal/cal-events/gh-pages/"
+    }
+}
