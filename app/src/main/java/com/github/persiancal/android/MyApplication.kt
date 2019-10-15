84036b6ddf61b3d8cfc8a4b1e6d99db2c8e78e18
Behrouz <bkhezry@gmail.com>
2019-10-16 01:19:24 +0330

    change style of app to material components day night - set default application night mode to NO

--- app/src/main/java/com/github/persiancal/android/MyApplication.kt
+++ app/src/main/java/com/github/persiancal/android/MyApplication.kt
@@ -1,11 +1,13 @@
 package com.github.persiancal.android
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.github.persiancal.androidsdk.CalendarEvents
 
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         CalendarEvents.init(this)
     }
 }
