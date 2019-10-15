b0b1927425d1d14a85e25a14ad1c2d9f7c878437
Behrouz <bkhezry@gmail.com>
2019-10-16 01:25:20 +0330

    add recycler view to main activity layout - init FastAdapter and ItemAdapter instance with JalaliEventItem class - setup recycler view options & set adapter to it - create JalaliEventItem instance with jalaliEvents and set to ItemAdapter

--- app/src/main/java/com/github/persiancal/android/MainActivity.kt
+++ app/src/main/java/com/github/persiancal/android/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.github.persiancal.android
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.aminography.primecalendar.PrimeCalendar
 import com.aminography.primecalendar.common.CalendarType
 import com.aminography.primedatepicker.OnDayPickedListener
 import com.aminography.primedatepicker.PickType
 import com.aminography.primedatepicker.calendarview.PrimeCalendarView
 import com.github.persiancal.androidsdk.CalendarEvents
+import com.mikepenz.fastadapter.FastAdapter
+import com.mikepenz.fastadapter.adapters.ItemAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+
 
 class MainActivity : AppCompatActivity(), OnDayPickedListener {
     override fun onDayPicked(
@@ -22,19 +26,38 @@ class MainActivity : AppCompatActivity(), OnDayPickedListener {
         if (pickType == PickType.SINGLE) {
             val dayOnMonth = singleDay!!.dayOfMonth
             val month = singleDay.month + 1
+            itemAdapter.clear()
             if (calendarEvents.isReady()) run {
                 val jalaliEvents = calendarEvents.getJalaliEvents(dayOnMonth, month)
-                Log.d("jalaliEvents", jalaliEvents!!.size.toString())
+                for (item in jalaliEvents!!) {
+                    val jalaliEventItem = JalaliEventItem(
+                        item.key,
+                        item.calendar,
+                        item.month,
+                        item.sources,
+                        item.year,
+                        item.description_fa_IR,
+                        item.title_fa_IR,
+                        item.day,
+                        item.holiday_Iran
+                    )
+                    itemAdapter.add(jalaliEventItem)
+                }
             }
         }
     }
 
     lateinit var calendarEvents: CalendarEvents
+    private val itemAdapter = ItemAdapter<JalaliEventItem>()
+    private lateinit var fastAdapter: FastAdapter<JalaliEventItem>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupCalendarView()
         calendarEvents = CalendarEvents.getInstance()
+        setupCalendarView()
+        setupRecyclerView()
+
     }
 
     private fun setupCalendarView() {
@@ -45,5 +68,13 @@ class MainActivity : AppCompatActivity(), OnDayPickedListener {
         calendarView.flingOrientation = PrimeCalendarView.FlingOrientation.HORIZONTAL
         calendarView.locale = Locale("fa")
         calendarView.onDayPickedListener = this
+    }
+
+    private fun setupRecyclerView() {
+        fastAdapter = FastAdapter.with(itemAdapter)
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.itemAnimator = DefaultItemAnimator()
+        recyclerView.adapter = fastAdapter
     }
 }
