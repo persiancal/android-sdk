bd85201dcb095290e17bacbbeadd6f5c3aed874e
Behrouz <bkhezry@gmail.com>
2019-10-16 02:10:19 +0330

    small change in converter function - if array has one item only store it

--- androidsdk/src/main/java/com/github/persiancal/androidsdk/util/StringListConverter.kt
+++ androidsdk/src/main/java/com/github/persiancal/androidsdk/util/StringListConverter.kt
@@ -12,6 +12,9 @@ class StringListConverter : PropertyConverter<List<String>, String> {
     override fun convertToDatabaseValue(entityProperty: List<String>?): String {
         if (entityProperty == null) return ""
         if (entityProperty.isEmpty()) return ""
+        if (entityProperty.size == 1) {
+            return entityProperty[0]
+        }
         val builder = StringBuilder()
         entityProperty.forEach { builder.append(it).append(",") }
         builder.deleteCharAt(builder.length - 1)
