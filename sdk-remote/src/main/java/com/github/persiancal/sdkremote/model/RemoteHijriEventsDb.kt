e69eb0cf0a0adf0271ced00472b4489e5b1b6ed6
Behrouz <bkhezry@gmail.com>
2019-10-17 01:24:23 +0330

    create RemoteHijriEventsDb class for store hijri events in local db - add generated json file of new class schema - //TODO extract common attribute as base class and extend classes from it

---
+++ sdk-remote/src/main/java/com/github/persiancal/sdkremote/model/RemoteHijriEventsDb.kt
@@ -0,0 +1,24 @@
+package com.github.persiancal.sdkremote.model
+
+import com.github.persiancal.sdkremote.util.StringListConverter
+import io.objectbox.annotation.Convert
+import io.objectbox.annotation.Entity
+import io.objectbox.annotation.Id
+
+@Entity
+data class RemoteHijriEventsDb(
+    @Id
+    var id: Long = 0,
+    val key: Long?,
+    @Convert(converter = StringListConverter::class, dbType = String::class)
+    val calendar: List<String?>? = null,
+    val month: Int? = null,
+    @Convert(converter = StringListConverter::class, dbType = String::class)
+    val sources: List<String?>? = null,
+    val year: Int? = null,
+    val description_fa_IR: String? = null,
+    val title_fa_IR: String? = null,
+    val day: Int? = null,
+    @Convert(converter = StringListConverter::class, dbType = String::class)
+    val holiday_Iran: List<String>? = null
+)
