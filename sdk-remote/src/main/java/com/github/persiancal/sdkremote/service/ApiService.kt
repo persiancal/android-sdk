22287adebb1c78ae25ef3e7ad6e5c5d6756f5bb0
Behrouz <bkhezry@gmail.com>
2019-10-18 23:30:44 +0330

    fix import part of EventsResponse class

--- sdk-remote/src/main/java/com/github/persiancal/sdkremote/service/ApiService.kt
+++ sdk-remote/src/main/java/com/github/persiancal/sdkremote/service/ApiService.kt
@@ -1,5 +1,6 @@
 package com.github.persiancal.sdkremote.service
 
+import com.github.persiancal.core.base.EventsResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -7,5 +8,5 @@ import retrofit2.http.Path
 interface ApiService {
 
     @GET("{endpoint}")
-    fun getJalaliEvents(@Path("endpoint") get: String): Single<com.github.persiancal.core.base.EventsResponse>
+    fun getJalaliEvents(@Path("endpoint") get: String): Single<EventsResponse>
 }
